9b455d277e2bd90b2a29cd27836447496c90e841
Zach Burkhart <zachburkhart216@gmail.com>
2022-07-28 16:24:21 -0400

    adding structure to index js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,26 @@
 var key = e3c849a0335ad07b5dedcf7b56f5457b
+var city = [];
 
+//date and time
 var today = moment().format('MM-DD-YYYY HH:MM:SS');
+
+//today's weather
+function weather() {
+    var apiUrl = 'https://api.openweathermap.org/data/2.5/onecall?q=${city}&appid=e3c849a0335ad07b5dedcf7b56f5457b';
+
+
+}
+
+//search history buttons
+var historyEl = 
+function history() {
+
+}
+
+
+//today card
+
+
+//five day forecast
+
 
